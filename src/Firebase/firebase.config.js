4a0fab38bca2156324a5a2a317703e1e07bab4cb
meer-habib-dev <contact.freelancermir@gmail.com>
2021-11-24 22:29:44 +0600

    ema client

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+console.log(process.env);
+const firebaseConfig = {
+  apiKey: "AIzaSyB2Q7ke-9FEJuzngOGRSqUEdTtJcMopJEQ",
+  authDomain: "ema-john-firebase-2fb5f.firebaseapp.com",
+  projectId: "ema-john-firebase-2fb5f",
+  storageBucket: "ema-john-firebase-2fb5f.appspot.com",
+  messagingSenderId: "869615560992",
+  appId: "1:869615560992:web:e564debefd772584224f45",
+  // apiKey: process.env.REACT_APP_API_KEY,
+  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_APP_ID,
+};
+
+export default firebaseConfig;
